afdb5dd41754b61cf7507e550a8d501acd471524
bonzo <40222661+baramatsubonzo@users.noreply.github.com>
2021-07-24 10:15:20 +0900

    Organismの作成1

---
+++ src/components/organisms/user/UserCard.jsx
@@ -0,0 +1,18 @@
+export const UserCard = () => {
+  return (
+    <div>
+      <img alt="プロフィール" />
+      <p>名前</p>
+      <dl>
+        <dt>メール</dt>
+        <dd>1111@example.com</dd>
+        <dt>TEL</dt>
+        <dd>000-0000-0000</dd>
+        <dt>会社名</dt>
+        <dd>sample株式会社</dd>
+        <dt>WEB</dt>
+        <dd>http.com</dd>
+      </dl>
+    </div>
+  );
+};
